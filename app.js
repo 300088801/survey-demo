82b248e084793a66ab6b909a5f9167c938a91761
Erich Osterman <300088801@fvtc.edu>
2024-03-26 19:29:16 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 (async () => {
 
-const { prompt } = require('enquirer')
-const axios = require('axios')
-
-
-
-
-
-
-
+const survey = require('./survey')
+const url = 'https://raw.githubusercontent.com/300088801/survey-demo/main/questions.json'
+survey.setUrl(url)
+console.log( await survey.run())
 
 })()
